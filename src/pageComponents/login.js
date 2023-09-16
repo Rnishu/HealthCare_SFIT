ef406d04b9a7813c553f05164e4fe4d0b7105c00
vaishnavi23-03 <103217074+vaishnavi23-03@users.noreply.github.com>
2023-09-16 06:19:46 +0530

    Update login.js

--- src/pageComponents/login.js
+++ src/pageComponents/login.js
@@ -2,9 +2,15 @@ import React from "react";
 
 function Login(){
     return(
-        <div>
+        <div classname="form">
+            <div classname="title">Welcome</div>
+            <div classname="subtitle">to BeWell</div>
+            <div class="input-container ic1">
             <input type="text" placeholder="Username"></input><br></br>
+            </div><div class="cut"></div>
+            <div class="input-container ic2">
             <input type="password" placeholder="Password"></input><br></br>
+            </div><div class="cut"></div>
             <button>Login</button>
             <a href='/register'>Register</a>
         </div>
